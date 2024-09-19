7594883d2f9c151f7c02efffa3e93640315646a9
Vyrnyl <aquinovern15@gmail.com>
2024-09-19 21:36:06 +0800

    client: folder structure

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>MAIN PAGE</h1>
+    </>
+  )
+}
+
+export default App
---
+++ server/prisma/migrations/20240918112053_add_units_col/migration.sql
@@ -0,0 +1,8 @@
+-- AlterTable
+ALTER TABLE `bsacurriculum` ADD COLUMN `units` INTEGER NOT NULL DEFAULT 3;
+
+-- AlterTable
+ALTER TABLE `bsbacurriculum` ADD COLUMN `units` INTEGER NOT NULL DEFAULT 3;
+
+-- AlterTable
+ALTER TABLE `bsmacurriculum` ADD COLUMN `units` INTEGER NOT NULL DEFAULT 3;
